705728c3d4e1c867e4aa010b9a0c3bff30bd0d54
Tharun Sekar <63772782+Tharun2331@users.noreply.github.com>
2026-05-18 15:06:53 -0400

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        triplet_set = set()
+        length = len(nums)
+        nums.sort()
+        for i in range(length):
+            hashset = set()
+            for j in range(i+1,length):
+                more_needed = - (nums[i] + nums[j])
+                if more_needed in hashset:
+                    temp = [nums[i],nums[j],more_needed]
+                    
+                    triplet_set.add(tuple(temp))
+
+                hashset.add(nums[j])
+        
+        ans = [list(trip) for trip in triplet_set]
+        return ans
+
+        
