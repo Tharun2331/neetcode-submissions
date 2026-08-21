013fdc28d5bdb3817a1de3f28526ffe7d786ec2e
Tharun Sekar <63772782+Tharun2331@users.noreply.github.com>
2026-08-21 19:06:05 -0400

    Add: top-k-elements-in-list - submission-11

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-11.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const count = {}
+        const freq = Array.from({length: nums.length+1}, () => [])
+
+        for(let num of nums) {
+            count[num] = (count[num] || 0) + 1
+        }
+
+        for(let n in count) {
+            freq[count[n]].push(parseInt(n))
+        }
+        const result = []
+        for(let i=freq.length-1; i>0; i--) {
+            for(let n of freq[i]) {
+                result.push(n)
+                if(result.length === k) {
+                return result
+            }
+            }
+            
+            
+        }
+
+
+
+
+
+    }
+}
